597d52c1a56b48b20b4d41ad7c7729a2905094f0
Jason J <jasonjinhang@hotmail.com>
2022-01-30 18:59:58 -0800

    correct daylight saving function

--- src/App.js
+++ src/App.js
@@ -45,18 +45,26 @@ class App extends Component {
 
     // re-render all the clock hands if days light saving was applied 
     if (this.state.dstApplied === "true") {
+      // Possibility One, if we need to use current time to show the days light saving, there should be nothing different now. So I'm going to take possibility two in this case
+
+      // Possibility One ============================================================================================================
       // update hours hand for local and london time, this time should have been automatially applied with days light saving
       // hour hands needed to be changed only
 
-      // change hour hand at local time zone, california
-      let dstLocalHours = currentTime.getHours() + handMinute / 60;
+      // // change hour hand at local time zone, california
+      // let dstLocalHours = currentTime.getHours() + handMinute / 60;
 
-      // change hour hand at London time zone
-      let londonCurrentTime = new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' });
-      // hanle string result of londonCurrentTime
-      let londonCurTime = londonCurrentTime.split(',')[1];
-      let londonDstHours = parseInt(londonCurTime.split(':')[0]) % 12 + londonMinutes / 60;
+      // // change hour hand at London time zone
+      // let londonCurrentTime = new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' });
+      // // hanle string result of londonCurrentTime
+      // let londonCurTime = londonCurrentTime.split(',')[1];
+      // let londonDstHours = parseInt(londonCurTime.split(':')[0]) % 12 + londonMinutes / 60;
 
+      // Possibility One ============================================================================================================
+
+      // Possibility Two, which assume it's under days light saving now, use UTC-7 for California, and UTC+1 for London time zone
+      let dstLocalHours = (currentTime.getUTCHours() - 7) + handMinute / 60;
+      let londonDstHours = (currentTime.getUTCHours() + 1) + handMinute / 60;
       // render the components with applying days light saving
       this.setState({hours: dstLocalHours});
       this.setState({londonHours: londonDstHours});
